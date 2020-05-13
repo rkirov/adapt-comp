a69276068a61f95902393839ea9e16e486414608
Rado Kirov <rkirov@gmail.com>
2020-05-12 21:31:21 -0700

    add one more example.

--- examples/simple_example.ts
+++ examples/simple_example.ts
@@ -1,5 +1,7 @@
 import {comp, read, write, pure} from '../simple'; 
+import {Modifiable} from '../adaptive'; 
 
+// example 1 - distance fn
 const x = comp(pure(1));
 const y = comp(pure(1));
 
@@ -15,3 +17,23 @@ console.log(op4.get());
 write(y, 3);
 
 console.log(op4.get());
+
+// example 2 - add up to n 
+
+// description
+function f(x: Modifiable<number>, y: Modifiable<number>): Modifiable<number> {
+  const nextX = comp(read(x, x => pure(x - 1)));
+  const nextY = comp(read(x, x => read(y, y => pure(x + y))));
+  const lessZ = comp(read(x, x => pure(x <= 0)));
+  return comp(read(lessZ, b => b ? read(y) : read(f(nextX, nextY))));
+}
+const x2 = comp(pure(10));
+const y2 = comp(pure(0));
+
+// computation
+const res2 = f(x2, y2);
+console.log(res2.get());  // returns 55;
+
+// re-computation
+write(y2, 10);
+console.log(res2.get());  // returns 155;
